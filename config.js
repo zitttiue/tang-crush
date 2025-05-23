5917912ded0b2da65df3f3745b3015efc05b6440
zitttiue <painsadno4@gmail.com>
2025-05-23 22:50:29 +0700

    muahacuaanhh
    iuemnhat

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    titleWeb: "Gửi emmm cô gái tháng 5",
+    introTitle: 'khvyyy iuuu dấu của anhh',
+    introDesc: `Trái tim đã sớm nguôi lạnh
+    nhưng khi gặp emmm nó lại đập mạnh một cách lạ thường
+    trong tim anhh là thứ zô cùng quý giá mà sao
+    emmm lại gỏn gọn trong trái tim anhh <33`,
+    btnIntro: '^^HiHi^^',
+    title: 'Emmm gặp anhh có phải là duyên số, Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 8 tỉ người mà sao em lại chon iuuu anhh <3',
+    btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Anhh biết mà 🥰. Yêu emmm nhiều nhiều lắm luôn 😘😘',
+    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
+    btnAccept: 'Oteeeee lun <3',
+    messLink:'https://www.facebook.com/thieen.nhzan?mibextid=wwXIfr&mibextid=wwXIfr' //link mess của các bạn. VD: /dinhkaito
 }
